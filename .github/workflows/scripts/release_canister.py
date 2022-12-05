d07c71183957499f386d3b1d46c57da48c595fc6
Akos Olasz <olaszakos@gmail.com>
2022-12-05 08:42:01 +0100

    clean up python scripts

--- .github/workflows/scripts/release_canister.py
+++ .github/workflows/scripts/release_canister.py
@@ -15,8 +15,6 @@ for v in ["DFX_IDENTITY_PREVIEW","POOL_CANISTER_ID"]:
     print(f"request_canister.py: {v} env variable missing")
     exit(1)
 
-
-
 private_key = base64.b64decode(os.environ["DFX_IDENTITY_PREVIEW"]).decode("utf-8")
 pool_id = os.environ["POOL_CANISTER_ID"]
 
@@ -30,35 +28,5 @@ def release_canister():
       pool_id, "release_canister", encode([{'type': Types.Text, 'value': sys.argv[1]}]))
   return res
 
-# print(whoami())
-# print(request_canister())
 
 release_canister()
-
-
-"""
-type Self = 
- service {
-   check_cycles: (text) -> (variant {
-                              err: text;
-                              ok: nat;
-                            });
-   get_pool: () -> (vec record {
-                          canister_id: principal;
-                          ref: opt text;
-                        }) query;
-   release_canister: (text) -> (variant {
-                                  err: text;
-                                  ok;
-                                });
-   request_canister: (text) -> (principal);
-   set_params: (InitParams) -> () oneway;
- };
-type InitParams = 
- record {
-   cycles_per_canister: nat;
-   max_canisters: opt nat;
-   min_cycles_per_canister: nat;
- };
-service : (InitParams) -> Self
-"""
